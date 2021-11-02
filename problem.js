b90f64de2134635c52f2e551fb4198e3b32b06f1
ShathaAlmansour <>
2021-11-02 16:31:04 +0300

    added file

--- problem.js
+++ problem.js
@@ -1,9 +1,16 @@
-const fizzBuzz = (x) => {
-for ( let x=0 ; x > 0 ; x++){
-    if ( )
-
-}
-
-}
-
-
+const fizzBuzz = (n) =>{
+    for (let x = 1; x<= n; x++) {   
+    if(x%3==0 && x%5==0){
+        console.log("fizzbuzz");
+    }
+    else if (x%3 === 0){
+        console.log("fizz");  
+    }
+    else if (x%5 ===0 ){
+    console.log("buzz");  }
+    else {
+        console.log(x);
+    }
+    
+    }}
+    fizzBuzz(30);
