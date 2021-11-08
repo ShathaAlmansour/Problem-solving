a273e06328b9cf2c918052cd257ebbc742da64fb
ShathaAlmansour <>
2021-11-09 00:04:12 +0300

    added

---
+++ isPrime.js
@@ -0,0 +1,12 @@
+function isPrime(n) {
+    if (n===1)
+   { return false; }
+   else if(n === 2) { return true;}
+   else {
+   for(let x = 2; x < n; x++)
+     { if(n % x === 0) {
+         return false; }
+     } return true;  
+   }}
+ console.log(isPrime(6)); // false
+ console.log(isPrime(7)); // true
