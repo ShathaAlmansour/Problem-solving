dbd798248a295cd5eb75b31ad610a890a33306d6
ShathaAlmansour <>
2021-11-10 11:29:16 +0300

    added

--- mathematicalReverse .js
+++ mathematicalReverse .js
@@ -5,3 +5,36 @@ function solve(i) {
     .join("");
   return mr;
 }
+
+// function solve(eq){
+//     let num="";
+//     let final = [];
+//     for(let i =0 ; i< eq.length ; i++)
+//       {
+//         if(eq[i]>= '0' && eq[i] <= '9' )
+//           {
+//             num+= eq[i] ;
+//           }
+//         else
+//           {
+//             if(num !="")
+//               {
+//                 final.push(num)
+//                 num="";
+//               }
+//             final.push(eq[i]);
+//           }
+//         }
+//     if(num !="")
+//       {
+//         final.push(num);
+//       }
+//      let result ="";
+//      final= final.reverse();
+//      console.log(final);
+//     for(let i =0 ; i< final.length ;i++)
+//       {
+//         result+=final[i];
+//       }
+//      return result
+//     }
